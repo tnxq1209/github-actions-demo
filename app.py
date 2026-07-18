385b862b71f8997b11e01ab342c7662ea05a6b51
Tanishq Kumar <156282991+tnxq1209@users.noreply.github.com>
2026-07-18 20:23:03 +0530

    Fix import and typos in app.py
    Fixed import statement and corrected typos in function definitions.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # a demo code to check if linter is working or not
-from flask imprt Flask, render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-df hello_world():
+def hello_world():
     return render_template('index.html')
     
 
 @app.route('/health')
 def health():
-    reurn 'Server is up and running'
+    return 'Server is up and running'
 # Running the linter.
