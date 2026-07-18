0b815af117342d4996418e7b7104c8bb5ca805f9
Tanishq Kumar <156282991+tnxq1209@users.noreply.github.com>
2026-07-18 20:21:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # a demo code to check if linter is working or not
-from flask import Flask, render_template
+from flask imprt Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+df hello_world():
     return render_template('index.html')
-
+    
 
 @app.route('/health')
 def health():
-    return 'Server is up and running'
+    reurn 'Server is up and running'
 # Running the linter.
